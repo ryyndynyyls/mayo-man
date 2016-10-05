05c519f6d9656c09c21947bc034365c41186f9a4
Jeff Sisson <jeffsisson@gmail.com>
2016-10-04 23:46:58 -0400

    pile of mayo

--- content.js
+++ content.js
@@ -1,10 +1,13 @@
-var mayo = chrome.extension.getURL('mayo.png');
 var isImage = /\.(jpg|jpeg|png|gif|svg)$/;
+
+function mayo() {
+	return chrome.extension.getURL('mayo' + (Math.floor(Math.random() * 6) + 1) + '.png')
+}
 
 chrome.webRequest.onBeforeRequest.addListener(function (details) {
 	if (isImage.test(details.url)) {
 	    return {
-	        redirectUrl: mayo
+	        redirectUrl: mayo()
 	    };
 	}
 }, {
@@ -16,7 +19,7 @@ chrome.webRequest.onHeadersReceived.addListener(function (details) {
 		if (details.responseHeaders[i].name.toLowerCase() === 'content-type') {
 			if (/image\//.test(details.responseHeaders[i].value)) {
 			    return {
-			        redirectUrl: mayo
+			        redirectUrl: mayo()
 			    };
 			}
 			break;
